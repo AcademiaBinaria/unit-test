91e6d0ba6b535f279eca7a2127e04b1358fe465a
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 13:05:06 +0100

    triangle initial spec

--- student/triangle.spec.js
+++ student/triangle.spec.js
@@ -1,9 +1,25 @@
+import { Triangle } from './triangle';
 /*
 FEATURE:    Determine if a triangle is equilateral, isosceles, or scalene.
 Aa a:       math student
 I want to:  check the kind of any triangle
 So:         I can learn about its geometry
 */
+
+// Scenario: I have a valid triangle with three sides
+describe('GIVEN: a Triangle class called with three legal values', () => {
+  // Arrange
+  const triangle = new Triangle(1, 2, 3);
+  describe('WHEN: i ask for the kind', () => {
+    //Act
+    const kind = triangle.kind();
+    const typeOfResult = typeof kind;
+    test('THEN: should return a string', () => {
+      // assert
+      expect(typeOfResult).toBe('string');
+    });
+  });
+});
 
 // Scenario: I have a valid triangle with three equal sides
 
