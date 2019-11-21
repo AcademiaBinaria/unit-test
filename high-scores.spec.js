41d881fe1b155f8093a8038b378002e1b122590d
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 11:45:24 +0100

    HighScores unit tested

--- high-scores.spec.js
+++ high-scores.spec.js
@@ -1,78 +1,62 @@
 import { HighScores } from './high-scores';
 
-// Feature: the app should allow me to create new tasks
-// As a user
-// I want to create new tasks
-// In order to do my work
-// Scenario: I am on the project list page
-// Given I have a list of projects
-// When I click on one of them
-// Then I should navigate to this project page
-
 /*
-Manage a game player's High Score list.
-
-Your task is to build a high-score component of the classic Frogger game,
-one of the highest selling and addictive games of all time, and a classic of the arcade era.
-Your task is to write methods that return the highest score from the list,
-the last added score and the three highest scores.
+FEATURE:  Manage a game player's High Score list.
+Aa a:     player with an array of scores
+I want:   to get the scores ordered, my last, my best, and my top three scores
+So:       I can track my progress
 */
 
-// Arrange
-// Act
-// Assert
-// After
-
-describe('GIVEN: a HighScores class', () => {
+// Scenario: I have a lot of scores
+describe('GIVEN: a HighScores class with a big array of scores', () => {
   // Arrange
-  const input = [30, 50, 20, 70];
+  const input = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 90, 70];
   const hs = new HighScores(input);
-  describe('WHEN: I send an array of scores', () => {
-    test('THEN: should return the list o f scores', () => {
-      expect(hs.scores) // act
-        .toEqual([30, 50, 20, 70]); // assert
+  describe('WHEN: I ask for the scores', () => {
+    //Act
+    const scores = hs.scores;
+    test('THEN: should return the list of scores ordered', () => {
+      // assert
+      expect(scores).toEqual([100, 90, 90, 70, 70, 40, 40, 30, 30, 30, 20, 10, 10, 0]);
+    });
+  });
+  describe('WHEN: I ask for Latest score', () => {
+    //Act
+    const latest = hs.latest;
+    test('THEN: should return the last one', () => {
+      // assert
+      expect(latest).toEqual(70);
+    });
+  });
+  describe('WHEN: I ask for the personal best', () => {
+    //Act
+    const personalBest = hs.personalBest;
+    test('THEN: should return the higher ', () => {
+      // assert
+      expect(personalBest).toEqual(100);
+    });
+  });
+  describe('WHEN: I ask for the top three', () => {
+    //Act
+    const personalTopThree = hs.personalTopThree;
+    test('THEN: should return the three higher scores', () => {
+      // assert
+      expect(personalTopThree).toEqual([100, 90, 90]);
     });
   });
 });
 
-describe('High Scores Test Suite', () => {
-  test('List of scores', () => {
-    const input = [30, 50, 20, 70];
-    expect(new HighScores(input).scores).toEqual([30, 50, 20, 70]);
-  });
-
-  test('Latest score', () => {
-    const input = [100, 0, 90, 30];
-    expect(new HighScores(input).latest).toEqual(30);
-  });
-
-  test('Personal best', () => {
-    const input = [40, 100, 70];
-    expect(new HighScores(input).personalBest).toEqual(100);
-  });
-
-  test('Personal top three from a list of scores', () => {
-    const input = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70];
-    expect(new HighScores(input).personalTopThree).toEqual([100, 90, 70]);
-  });
-
-  test('Personal top highest to lowest', () => {
-    const input = [20, 10, 30];
-    expect(new HighScores(input).personalTopThree).toEqual([30, 20, 10]);
-  });
-
-  test('Personal top when ther eis a tie', () => {
-    const input = [40, 20, 40, 30];
-    expect(new HighScores(input).personalTopThree).toEqual([40, 40, 30]);
-  });
-
-  test('Personal top when there are less than 3', () => {
-    const input = [30, 70];
-    expect(new HighScores(input).personalTopThree).toEqual([70, 30]);
-  });
-
-  test('Personal top when there is only one', () => {
-    const input = [40];
-    expect(new HighScores(input).personalTopThree).toEqual([40]);
+// Scenario: I have just started with the game
+describe('GIVEN: a HighScores class with a small array of scores', () => {
+  // Arrange
+  const input = [30, 70];
+  const hs = new HighScores(input);
+  describe('WHEN: I ask for the top three', () => {
+    //Act
+    const personalTopThree = hs.personalTopThree;
+    test('THEN: should return all the scores', () => {
+      // assert
+      expect(personalTopThree).toEqual([70, 30]);
+    });
   });
 });
