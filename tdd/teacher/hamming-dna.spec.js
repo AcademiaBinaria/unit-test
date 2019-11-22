b575c95cc8e438591989028212108b0dc3b8e401
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-22 10:01:20 +0100

    fix typo

---
+++ tdd/teacher/hamming-dna.spec.js
@@ -0,0 +1,10 @@
+/*
+FEATURE:    Calculate the Hamming Distance between two DNA strands.
+As a:       biologist studying cell divisions
+I want to:  compare two strands of DNA and count the differences between them
+So:         I can see how many mistakes occurred.
+*/
+
+// Scenario: we have two valid sequences of equal length
+
+// Scenario: we have two invalid sequences of different lengths
